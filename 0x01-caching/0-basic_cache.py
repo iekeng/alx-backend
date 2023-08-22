bb4df4e3ad3bb9fb86318f0a31b3b8a573709f1b
root <root@DESKTOP-QS0QAE8>
2023-08-22 21:10:38 +0100

    initial commit

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3i
+'''0-basic_cache'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''Inherits all methods from the BaseCaching class'''
+
+    def put(self, key: str, item: str):
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key: str):
+        try:
+            return self.cache_data[key]
+        except KeyError:
+            return None
