98abe327ed6ecadc12573043e0bf64cca121ee49
root <root@DESKTOP-QS0QAE8>
2023-08-23 08:17:41 +0100

    pep8 compliant style

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
+'''1-fifo_cache'''
 
-BaseCaching=__import__('base_caching').BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    '''Implements FIFO cache algorithm'''
+
     def __init__(self):
+        '''Inherits methods and
+        properties from parent class'''
         super().__init__()
 
     def put(self, key, item):
+        '''Sets a new item in cache_data dict'''
         if key is None or item is None:
             return
         self.cache_data[key] = item
@@ -15,8 +21,9 @@ class FIFOCache(BaseCaching):
             wanted_key = list(self.cache_data)[0]
             self.cache_data.pop(wanted_key)
             print(f"DISCARD {wanted_key}")
-    
+
     def get(self, key):
+        '''Retrieves item from cache_data dict'''
         try:
             return self.cache_data[key]
         except KeyError:
