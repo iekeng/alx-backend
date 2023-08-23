b190737d422db51eb9922c5896a3e7c5130c1010
root <root@DESKTOP-QS0QAE8>
2023-08-23 12:04:22 +0100

    err fics

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -5,20 +5,29 @@ BaseCaching=__import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
+    '''implements LIFO cache replacement policy'''
     def __init__(self):
+        '''inherits from the super class'''
         super().__init__()
 
     def put(self, key, item):
+        '''Sets an item in the cache_data dict'''
         if key is None or item is None:
             return
+
+        elif len(self.cache_data) >= self.MAX_ITEMS and key not in self.cache_data:
+            newest_key = list(self.cache_data)[-1]
+            self.cache_data.pop(newest_key)
+            print(f'DISCARD: {newest_key}')
+
+        elif key in self.cache_data:
+            self.cache_data[key] = item
+            print(f'DISCARD: {key}')
+
         self.cache_data[key] = item
-        C:\Users\STELLA\Desktop\Zoom.lnk
-        if len(self.cache_data) > self.MAX_ITEMS:
-            wanted_key = list(self.cache_data)[-2]
-            self.cache_data.pop(wanted_key)
-            print(f"DISCARD: {wanted_key}") 
 
     def get(self, key):
+        '''retrives an item from the cache_data dict'''
         try:
             return self.cache_data[key]
         except KeyError:
