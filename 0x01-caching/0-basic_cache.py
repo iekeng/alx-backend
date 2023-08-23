7aeb4f6b4792b47a0225e1c132f3dc5ec65cbff7
root <root@DESKTOP-QS0QAE8>
2023-08-23 07:56:42 +0100

    added comments to functions

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,11 +8,13 @@ class BasicCache(BaseCaching):
     '''Inherits all methods from the BaseCaching class'''
 
     def put(self, key: str, item: str):
+        '''sets an item in the cache_data dict'''
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key: str):
+        '''retrieves an item from the cache_data dict'''
         try:
             return self.cache_data[key]
         except KeyError:
