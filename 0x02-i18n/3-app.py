42b1be5f211b392843e9a92fb06781ab48d25561
root <root@DESKTOP-QS0QAE8>
2023-08-29 15:44:23 +0100

    translations

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Select and return best language match based on supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Handles / route
+    """
+    home_title = gettext('home_title')
+    home_header = gettext('home_header')
+    return render_template('3-index.html', home_title=home_title, home_header=home_header)
