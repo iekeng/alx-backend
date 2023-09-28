33e8b51df4de0a9844be0f35e4ed8063a4055d40
iekeng <ekeng7.eb@gmail.com>
2023-09-28 08:09:02 +0000

    err fix and job prcessor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ import { createQueue } from 'kue';
 const queue = createQueue();
 
 const notification = {
-  phoneNumber: '123456789',
-  message: 'Push notification',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 }
 
 const job = queue.create('push_notification_code', notification).save((err) => {
